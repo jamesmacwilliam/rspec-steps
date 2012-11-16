386b9127446e1ed5ac004c30630469bb3f529460
James Mac William <jmacwilliam@constantcontact.com>
2012-11-16 15:59:12 -0500

    commit with just stepwise update

--- lib/rspec-steps/stepwise.rb
+++ lib/rspec-steps/stepwise.rb
@@ -124,11 +124,13 @@ module RSpecStepwise
             break if RSpec.wants_to_quit
             unless success
               reporter.example_started(example)
+              unless example.metadata[:pend_on_failure] || !example.metadata.has_key?(:pend_on_failure)
               example.metadata[:pending] = true
-              example.metadata[:execution_result][:pending_message] = "Previous step failed"
-              example.metadata[:execution_result][:started_at] = Time.now
-              example.instance_eval{ record_finished :pending, :pending_message => "Previous step failed" }
-              reporter.example_pending(example)
+                example.metadata[:execution_result][:pending_message] = "Previous step failed"
+                example.metadata[:execution_result][:started_at] = Time.now
+                example.instance_eval{ record_finished :pending, :pending_message => "Previous step failed" }
+                reporter.example_pending(example)
+              end
               next
             end
             succeeded = instance.with_indelible_ivars do
